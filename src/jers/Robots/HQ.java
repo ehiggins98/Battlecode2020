1832a1d5b1af775fa2f551230501ee473f920d55
Ryan Pope <poperyan73@gmail.com>
2020-01-12 13:45:32 -0600

    Started using the Net Gun in HQ

--- src/jers/Robots/HQ.java
+++ src/jers/Robots/HQ.java
@@ -23,6 +23,9 @@ public class HQ extends Robot {
             locationBroadcast = true;
         }
 
+        // Check if drones are nearby and use net gun
+        useNetGun();
+
         switch (goal) {
             case BUILD_INITIAL_MINERS:
                 buildInitialMiners();
@@ -62,6 +65,32 @@ public class HQ extends Robot {
 
         if (robotBuiltMessage != null && transactor.submitTransaction(robotBuiltMessage)) {
             robotBuiltMessage = null;
+        }
+    }
+
+    private void useNetGun() throws GameActionException {
+        // Want to find nearest robots
+        RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED, rc.getTeam().opponent());
+        if (robots.length != 0)
+        {
+            RobotInfo minDistEnemy = robots[0];
+            int minDistance = rc.getLocation().distanceSquaredTo(minDistEnemy.location);
+            for (RobotInfo robot : robots) {
+                if (robot.type != RobotType.DELIVERY_DRONE)
+                {
+                    continue;
+                }
+
+                int newDist = rc.getLocation().distanceSquaredTo(robot.location);
+                if (newDist < minDistance) {
+                    minDistance = newDist;
+                    minDistEnemy = robot;
+                }
+            }
+
+            if (rc.canShootUnit(minDistEnemy.ID)) {
+                rc.shootUnit(minDistEnemy.ID);
+            }
         }
     }
 }
